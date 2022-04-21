05b1b0583530b478c943a5b35ff8f41f69ff26bb
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-21 12:10:46 -0400

    Completed Read me

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 // Code your solutions in this file
 const guest = ['Lisa', 'Kaitlin', 'Jan']
+const message = []
 
 function writeCards(attendees, event) {
     for (let i = 0; i < attendees.length; i++) {
-        console.log(`Thank you ${attendees[i]}, for the wonderful ${event} gift!`);
+       message.push(`Thank you, ${attendees[i]}, for the wonderful ${event} gift!`);
+    }
+    return message;
+}
+
+
+
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer--);
     }
 }
 
-writeCards(guest, 'surprise')
-
-
-
+countDown(10);
 
