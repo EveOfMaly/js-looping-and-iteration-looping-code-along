43c876fe981a2e4069fe67dc9f49da1093d22275
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-21 11:37:54 -0400

    WriteCards test error

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const guest = ['Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(attendees, event) {
+    for (let i = 0; i < attendees.length; i++) {
+        console.log(`Thank you ${attendees[i]}, for the wonderful ${event} gift!`);
+    }
+}
+
+writeCards(guest, 'surprise')
+
+
+
+
